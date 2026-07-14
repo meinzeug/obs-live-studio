b8e8e6f4b1de59de744a74caf53be68856916486
meinzeug <dennis.wicht@web.de>
2026-07-14 16:24:19 +0000

    Normalize source updates atomically

--- packages/database/src/010_source_url_state.sql
+++ packages/database/src/010_source_url_state.sql
@@ -1,8 +1,14 @@
-create or replace function reset_source_fetch_state_on_url_change()
+create or replace function normalize_source_update_state()
 returns trigger
 language plpgsql
 as $$
 begin
+  if new.user_agent = '' then
+    new.user_agent := null;
+  elsif new.user_agent is null and old.user_agent is not null then
+    new.user_agent := old.user_agent;
+  end if;
+
   if new.url is distinct from old.url then
     new.etag := null;
     new.last_modified := null;
@@ -15,8 +21,9 @@ end;
 $$;
 
 drop trigger if exists sources_reset_fetch_state_on_url_change on sources;
+drop trigger if exists sources_normalize_update on sources;
 
-create trigger sources_reset_fetch_state_on_url_change
-before update of url on sources
+create trigger sources_normalize_update
+before update on sources
 for each row
-execute function reset_source_fetch_state_on_url_change();
+execute function normalize_source_update_state();

910acd8404ff38854c715557e88fa4e3f2680fa7
meinzeug <dennis.wicht@web.de>
2026-07-14 21:24:43 +0000

    Register article visual media migration

--- packages/database/src/migrate.ts
+++ packages/database/src/migrate.ts
@@ -15,6 +15,7 @@ const migrationFiles = [
   '009_source_health.sql',
   '010_source_url_state.sql',
   '011_live_event_dedupe.sql',
+  '012_article_visual_media.sql',
 ];
 async function readFirst(name: string) {
   const candidates = [

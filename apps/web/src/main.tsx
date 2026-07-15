2efbf1c80b3215352a1ed51260dd1a0e81cadec2
meinzeug <dennis.wicht@web.de>
2026-07-15 05:56:35 +0000

    Load navigation interaction styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import './navigation.css';
 import { App } from './App.js';
 createRoot(document.getElementById('root')!).render(<App />);
